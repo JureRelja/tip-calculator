f256e723342a1ac046ba5d2070993f9b04a62a5f
JureRelja <jurereljanovic99@gmail.com>
2023-01-29 22:02:05 +0100

    finished

---
+++ README.md
@@ -0,0 +1,44 @@
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX).
+
+It was a fun challenge building this simple tip calculator app. I tried to make it work as close to the original design as posible.
+
+## Table of contents
+
+- [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+
+### The challenge
+
+All users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+- Make calculations happend whenever one parameter is changed
+- Reset the whole app with the RESET button
+
+### Screenshot
+
+![](./src/assets/tip-calculator-desktop.jpg)
+![](./src/assets/tip-calculator-mobile.jpg)
+
+### Links
+
+- Live Site URL: https://jure-tip-calculator.netlify.app/
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- [React](https://reactjs.org/)
+
+### What I learned
+
+Adding red alerts when user's input is wrong happend to be little tricky, but I managed to do it. Other things were fairly straight forward, like optimizing the app to work on various screen sizes and making the app recalculate everything again when user changes one input. Interestingly, adding .svg icon into the input field required a little bit more effort than I expected, but I did it without many isuess.
+
+The project is small, but still a decent challenge if you give yourself a fixed amount of time to complete it.
--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,47 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  //Variable for calcualting tip
   const [tip, setTip] = useState(0);
   const [tipPerPerson, setTipPerPerson] = useState((0.0).toFixed(2));
   const [totalTip, setTotalTip] = useState((0.0).toFixed(2));
   const [bill, setBill] = useState(0);
   const [people, setPeople] = useState(0);
 
+  //Colored alerts for bill input
+  const [billAlert, setBillAlert] = useState("hidden");
+  const [billOutline, setBillOutline] = useState("#26C0AB");
+  const [billBaseOutline, setBillBaseOutline] = useState("#F4FAFA");
+
+  //Colored alerts for people input
+  const [peopleAlert, setPeopleAlert] = useState("hidden");
+  const [peopleOutline, setPeopleOutline] = useState("#26C0AB");
+  const [PeopleBaseOutline, setPeopleBaseOutline] = useState("#F4FAFA");
+
+  //Colored alerts for tip input
+  const [tipOutline, setTipOutline] = useState("#26C0AB");
+  const [tipBaseOutline, setTipBaseOutline] = useState("#F4FAFA");
+
   useEffect(() => {
     if (bill > 0 && people > 0) {
-      setTotalTip(Math.round((bill * tip) / 100).toFixed(2));
-      setTipPerPerson(Math.round((bill * tip) / 100 / people).toFixed(2));
+      setTotalTip(((bill * tip) / 100).toFixed(2));
+      setTipPerPerson(((bill * tip) / 100 / people).toFixed(2));
     }
   }, [tip, bill, people]);
 
   return (
-    <div className="bg-[#C5E4E7] h-[100vh] flex justify-center items-center flex-col">
+    <div className="bg-[#C5E4E7] flex justify-center items-center flex-col pt-20 md:pt-0 h-[100vh] md:min-h-0 min-h-[900px]">
       <img src="./src/assets/logo.svg" className="mb-12" />
-      <div className="bg-white rounded-xl flex flex-row p-8 shadow-xl space-x-10">
+      <div className="bg-white rounded-xl flex flex-col p-8 shadow-xl space-y-7 md:space-y-0 md:flex-row md:space-x-10">
         <div className="flex flex-col space-y-10 w-[300px]">
           <label className="flex flex-col items-start ">
-            <span className="mb-1 text-[#5E7A7D] text-[14px]">Bill</span>
+            <div className="flex justify-between items-center w-full mb-1">
+              <span className=" text-[#5E7A7D] text-[14px]">Bill</span>
+              <span className={`text-[#f16e6e] text-[14px] ${billAlert} `}>
+                Can't be zero
+              </span>
+            </div>
+
             <div className="flex flex-row items-center w-full">
               <img
                 src="./src/assets/icon-dollar.svg"
@@ -29,10 +50,22 @@ function App() {
               />
               <input
                 type="number"
-                className="bg-[#F4FAFA] text-right text-[#00494D] h-full transition-all ease-in-out  w-full hover:outline focus:outline outline-[#26C0AB] outline-1 rounded-md px-3 py-1 placeholder:text-[#7F9C9F] placeholder:text-[18px] text-[18px]"
+                className={`bg-[#F4FAFA] text-right text-[#00494D] h-full transition-all ease-in-out duration-75 w-full hover:outline-[${billOutline}] focus:outline-[${billOutline}] outline-[${billBaseOutline}] outline outline-1 rounded-md px-3 py-1 placeholder:text-[#7F9C9F] placeholder:text-[18px] text-[18px]`}
                 placeholder="0"
                 value={bill === 0 ? "" : bill}
                 onChange={(e) => setBill(e.target.value)}
+                onBlur={() => {
+                  if (bill == 0) {
+                    setBillAlert("block");
+                    setBillOutline("#f16e6e");
+                    setBillBaseOutline("#f16e6e");
+                  }
+                }}
+                onFocus={() => {
+                  setBillAlert("hidden");
+                  setBillOutline("#26C0AB");
+                  setBillBaseOutline("#F4FAFA");
+                }}
               />
             </div>
           </label>
@@ -40,7 +73,7 @@ function App() {
             <span className="mb-1 text-[#5E7A7D] text-[14px]">
               Select tip %
             </span>
-            <div className="grid grid-rows-2 grid-cols-3 space gap-4 mt-1">
+            <div className="grid grid-rows-2 grid-cols-2 space gap-4 mt-1 md:grid-cols-3">
               <input
                 type="submit"
                 value="5%"
@@ -83,35 +116,63 @@ function App() {
               />
               <input
                 type="number"
-                className="bg-[#F4FAFA] rounded-md text-[#00494D] px-3 text-right placeholder:text-[#00494D] placeholder:text-center hover:outline focus:outline outline-1 outline-[#26C0AB] transtion-all ease-int-out"
+                className={`bg-[#F4FAFA] rounded-md text-[#00494D] px-3 text-right placeholder:text-[#00494D] placeholder:text-center hover:outline-[${tipOutline}] focus:outline-[${tipOutline}] outline-[${tipBaseOutline}] outline outline-1 transtion-all ease-int-out `}
                 placeholder="Custom"
                 value={tip === 0 ? "" : tip}
                 onChange={(e) => {
                   setTip(e.target.value);
                 }}
+                onBlur={() => {
+                  if (tip == 0) {
+                    setTipOutline("#f16e6e");
+                    setTipBaseOutline("#f16e6e");
+                  }
+                }}
+                onFocus={() => {
+                  setTipOutline("#26C0AB");
+                  setTipBaseOutline("#F4FAFA");
+                }}
               />
             </div>
           </div>
           <label className="flex flex-col items-start">
-            <span className="mb-1 text-[#5E7A7D] text-[14px]">
-              Number of people
-            </span>
+            <div className="flex justify-between items-center w-full mb-1">
+              <span className=" text-[#5E7A7D] text-[14px]">
+                Number of people
+              </span>
+              <span className={`text-[#f16e6e] text-[14px] ${peopleAlert} `}>
+                Can't be zero
+              </span>
+            </div>
+
             <div className="flex flex-row items-center w-full">
               <img
                 src="./src/assets/icon-person.svg"
-                className="h-[16px] ml-3 absolute pointer-events-none"
+                className="h-[16px] ml-3  pointer-events-none absolute"
               />
               <input
                 type="number"
-                className={`peer bg-[#F4FAFA] text-right text-[18px] text-[#00494D] h-full transition-all ease-in-out w-full hover:outline focus:outline outline-[#26C0AB] outline-1 rounded-md px-3 py-1 placeholder:text-[#7F9C9F] placeholder:text-[18px]`}
+                className={`peer bg-[#F4FAFA] text-right text-[18px] text-[#00494D] transition-all duration-75 h-full w-full hover:outline-[${peopleOutline}] focus:outline-[${peopleOutline}] outline-[${PeopleBaseOutline}] outline outline-1 rounded-md px-3 py-1 placeholder:text-[#7F9C9F] placeholder:text-[18px]`}
                 placeholder="0"
                 value={people === 0 ? "" : people}
                 onChange={(e) => setPeople(e.target.value)}
+                onBlur={() => {
+                  if (people == 0) {
+                    setPeopleAlert("block");
+                    setPeopleOutline("#f16e6e");
+                    setPeopleBaseOutline("#f16e6e");
+                  }
+                }}
+                onFocus={() => {
+                  setPeopleAlert("hidden");
+                  setPeopleOutline("#26C0AB");
+                  setPeopleBaseOutline("#F4FAFA");
+                }}
               />
             </div>
           </label>
         </div>
-        <div className="bg-[#00494D] rounded-lg w-[300px] flex flex-col space-y-2 items-center h-full px-6 py-8">
+        <div className="bg-[#00494D] rounded-lg flex flex-col space-y-2 items-center px-6 py-8 w-[300px]">
           <div className="flex flex-row basis-1/4 justify-between w-full">
             <div>
               <h2 className="text-white">Tip Amount</h2>
@@ -133,10 +194,11 @@ function App() {
               setTip(0);
               setTipPerPerson((0.0).toFixed(2));
               setTotalTip((0.0).toFixed(2));
+              setAlert("hidden");
             }}
             value="RESET"
             disabled={bill === 0.0 && people === 0 && tip === 0}
-            className="bg-[#26C0AB] text-[18px] text-[#00494D] disabled:cursor-default  disabled:bg-[#C5E4E7] disabled:opacity-40 rounded-md p-1 w-full hover:bg-[#C5E4E7] hover:text-[#00494D] transtion-all ease-int-out cursor-pointer"
+            className="bg-[#26C0AB] text-[18px] text-[#00494D] disabled:cursor-default disabled:bg-[#C5E4E7] disabled:opacity-40 rounded-md p-1 w-full hover:bg-[#C5E4E7] hover:text-[#00494D] transtion-all ease-int-out cursor-pointer"
           />
         </div>
       </div>
